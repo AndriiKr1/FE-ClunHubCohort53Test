38e4a9ac9168d111059be660515a7abb9b1bfc5f
AndriiKr1 <shadowuals@gmail.com>
2025-04-01 18:55:02 +0200

    18

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Provider } from 'react-redux'; // Додаємо Provider для Redux
-import store from './store/store'; // Імпортуємо ваш Redux store
 import WelcomePage from './components/WelcomePage/WelcomePage';
 import RegisterPage from './components/Registration/RegisterPage';
 import LoginPage from './components/Login/LoginPage';
 import ForgotPasswordPage from './components/ForgotPassword/ForgotPasswordPage';
-import Calendar from './components/Calendar/Calendar'; // Імпортуємо Calendar
+import Dashboard from './components/Dashboard/Dashboard';
+import AddTask from './components/AddTask/AddTask';
 
-// Заглушки для майбутніх компонентів
-
-const Dashboard = () => <div>Dashboard Page</div>;
 
 function App() {
   return (
-    <Provider store={store}> {/* Обгортаємо додаток у Provider */}
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} /> 
-        <Route path="/dashboard" element={<Calendar />} /> {/* Можна залишити і тут */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addtask" element={<AddTask />} />
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
-    </Provider>
   );
 }
 
